b14f87e111281110b3842c19bd785b6f2a77e8cb
rungokarol <karolrungo@gmail.com>
2018-03-06 20:41:34 +0100

    minify js and minify images plugins for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,10 @@ const rename = require('gulp-rename')
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCss = require('gulp-clean-css');
-const uglyfile = require('gulp-uglify');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
 const browserSync = require('browser-sync');
 
 gulp.task('reload', function(){
@@ -26,24 +29,36 @@ gulp.task('sass', function(){
 });
 
 gulp.task('min-css', function(){
-    return gulp.src('./css/**/*.css')
+    return gulp.src('css/**/*.css')
+                .pipe(concat('style.css'))
                 .pipe(cleanCss())
                 .pipe(rename('style.min.css'))
-                .pipe(gulp.dest('./css'));
+                .pipe(gulp.dest('css'));
 });
 
 gulp.task('ugly-js', function(){
-    return gulp.src('./js/**/*.js')
-                .pipe(uglyfile())
-                .pipe(rename('main.ugly.js'))
-                .pipe(gulp.dest('./js'));
+    return gulp.src('js/**/*.js')
+                .pipe(concat('script.js'))
+                .pipe(uglify().on('error', function(e){
+                    console.log(e);
+                }))
+                .pipe(rename('script.ugly.js'))
+                .pipe(gulp.dest('js'));
+});
+
+gulp.task('min-img', function(){
+    return gulp.src('img/**/*.{jpg,jpeg,png,gif}')
+                .pipe(changed('img-min'))
+                .pipe(imagemin())
+                .pipe(gulp.dest('img-min'))
+
 });
 
 gulp.task('run', ['sass'], function(){
     browserSync({
         server: './'
     });
-    gulp.watch('./*.html', ['reload']);
+    gulp.watch('*.html', ['reload']);
     gulp.watch('sass/**/*.scss', ['sass']);
 });
 
